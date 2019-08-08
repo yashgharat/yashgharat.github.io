a8d63bdb97639a586d4bcc0696706ea3c5a7473c
Yash Gharat <yashgharat12@gmail.com>
2019-08-08 10:19:43 -0400

    responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 (function ($) {
 	"use strict";
 
-
+	$(window).trigger('scroll');
+	$(window).on('scroll', function () {
+		var pixels = 50;
+		var top = 1200;
+		if ($(window).scrollTop() > pixels) {
+			$('.navbar-expand-md').addClass('navbar-reduce');
+			$('.navbar-expand-md').removeClass('navbar-trans');
+		} else {
+			$('.navbar-expand-md').addClass('navbar-trans');
+			$('.navbar-expand-md').removeClass('navbar-reduce');
+		}
+		if ($(window).scrollTop() > top) {
+			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+		} else {
+			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+		}
+	});
+	
 		/*--/ Star Typed /--*/
 	if ($('.text-slider').length == 1) {
 	var typed_strings = $('.text-slider-items').text();
