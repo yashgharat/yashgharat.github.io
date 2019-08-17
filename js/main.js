5c4765f5965218ba1b0921aed53dd9ed0db874a2
Yash Gharat <yashgharat12@gmail.com>
2019-08-16 21:32:36 -0400

    cube

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-(function ($) {
+(function($) {
 	"use strict";
 	var nav = $('nav');
 	var navHeight = nav.outerHeight();
 
 	$(window).trigger('scroll');
-	$(window).on('scroll', function () {
+	$(window).on('scroll', function() {
 		var pixels = 50;
 		var top = 1200;
 		if ($(window).scrollTop() > pixels) {
@@ -37,12 +37,12 @@
 		scrollOffset: 0
 	});
 
-	$(".navbar-collapse a").on('click', function () {
+	$(".navbar-collapse a").on('click', function() {
 		$(".navbar-collapse.collapse").removeClass('in');
 	});
 
 
-	$(window).scroll(function () {
+	$(window).scroll(function() {
 
 		var scrollTop = $(window).scrollTop();
 
@@ -66,20 +66,20 @@
 
 	}
 
-	$(document).ready(function () {
+	$(document).ready(function() {
 		var browserWidth = $(window).width();
 		if (browserWidth > 560) {
-			$(window).scroll(function () {
+			$(window).scroll(function() {
 				navbar();
 			});
 		}
 	});
 
 
-	$(window).resize(function () {
+	$(window).resize(function() {
 		var browserWidth = $(window).width();
 		if (browserWidth > 560) {
-			$(window).scroll(function () {
+			$(window).scroll(function() {
 				navbar();
 			});
 		}
@@ -90,3 +90,24 @@
 	new WOW().init();
 
 })(jQuery);
+
+//DOM load event
+window.addEventListener("DOMContentLoaded", () => {
+	const cube = document.querySelector(".cube"),
+		imageButtons = document.querySelector(".image-buttons");
+	let cubeImageClass = cube.classList[1];
+	//Add click event listener to image buttons container
+	imageButtons.addEventListener("click", e => {
+		//Get node type and class value of clicked element
+		const targetNode = e.target.nodeName,
+			targetClass = e.target.className;
+		//Check if image input has been clicked and isn't the currently shown image
+		if (targetNode === "INPUT" && targetClass !== cubeImageClass) {
+			console.log(`Show Image: ${targetClass.charAt(11)}`);
+			//Replace previous cube image class with new class
+			cube.classList.replace(cubeImageClass, targetClass);
+			//Update cube image class variable with new class
+			cubeImageClass = targetClass;
+		}
+	});
+});
