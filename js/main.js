879dc5ccc78a54001e0b3389d91e0223cc239538
Yash Gharat <yashgharat12@gmail.com>
2019-08-18 22:13:06 -0400

    staged

--- js/main.js
+++ js/main.js
@@ -86,6 +86,29 @@
 		}
 	});
 
+	$('.js-scroll').on("click", function() {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+
+	$(window).trigger('scroll');
+	$(window).on('scroll', function() {
+		var pixels = 50;
+		var top = 1200;
+		if ($(window).scrollTop() > pixels) {
+			$('.navbar-expand-md').addClass('navbar-reduce');
+			$('.navbar-expand-md').removeClass('navbar-trans');
+		} else {
+			$('.navbar-expand-md').addClass('navbar-trans');
+			$('.navbar-expand-md').removeClass('navbar-reduce');
+		}
+		if ($(window).scrollTop() > top) {
+			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+		} else {
+			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+		}
+	});
+
 	// $('body').scrollspy({ target: '#navbarDefault', offset: 50 });
 
 	new WOW().init();
@@ -93,6 +116,6 @@
 })(jQuery);
 
 
-baguetteBox.run('.gallery',{
-	fullscreen: false, 
+baguetteBox.run('.gallery', {
+	fullscreen: false,
 });
