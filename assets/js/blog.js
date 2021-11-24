429b636b66d8d19b47cc57bfc1b9ad89575fd7d5
Yash Gharat <yashgharat12@gmail.com>
2021-11-24 12:14:43 -0500

    loading new website

---
+++ assets/js/blog.js
@@ -0,0 +1,118 @@
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "gbh9lcdequj5",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: "2fK7lWbeV2bWHfWGT7_3HcGTVh27or7cQ4bdIrRzrxo",
+});
+
+const converter = initHTMLConverter();
+
+window.addEventListener("load", () => {
+  var archiveList = document.getElementById("postsArchive");
+
+  client
+    .getEntries({
+      content_type: "blogPost",
+      order: "-fields.publishDate",
+    })
+    .then((entries) => {
+      postArr = entries.items;
+      postArr.forEach((entry) => {
+        console.log(entry);
+        var item = document.createElement("li");
+        item.appendChild(document.createTextNode(entry.fields.title));
+        item.setAttribute("class", "archive-item");
+        item.setAttribute("id", entry.sys.id);
+
+        archiveList.append(item);
+
+        $("#" + entry.sys.id).click(() => {
+            clickedPost = postMap.get(entry.sys.id);
+            document.getElementById("blogModalTitle").innerHTML =
+              clickedPost.title;
+            document.getElementById("blogModalBody").innerHTML =
+              converter.makeHtml(clickedPost.body);
+            $("#blogModal").modal("show");
+          });
+      });
+
+      postMap = new Map(postArr.map((post) => [post.sys.id, post.fields]));
+
+      for (let i = 1; i < 4; i++) {
+        fillPost(postArr[i - 1], i);
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+
+// Helper Functions
+
+function initHTMLConverter() {
+  showdown.setFlavor("github");
+  converter_ = new showdown.Converter();
+
+  converter_.setOption("headerLevelStart", 3);
+  converter_.setOption("simpleLineBreaks", true);
+  converter_.setOption("openLinksInNewWindow", true);
+  converter_.setOption("tables", true);
+  converter_.setOption("parseImgDimensions", true);
+  converter_.setOption("disableForced4SpacesIndentedSublists", true);
+  converter_.setOption("emoji", true);
+
+  return converter_;
+}
+
+function fillPost(post, i) {
+  // post is from Contentful, i is for html (no increment needed)
+
+  container = document.getElementById("postContainer-" + i);
+  title = document.getElementById("postTitle-" + i);
+  date = document.getElementById("postDate-" + i);
+  desc = document.getElementById("postDesc-" + i);
+  time = document.getElementById("postTime-" + i);
+  tags = document.getElementById("postTags-" + i);
+
+  var updatedAt = new Date(post.sys.updatedAt).toISOString().split("T")[0];
+
+  postTags = Array.from(post.fields.tags)
+    .map((tag) => "#" + tag)
+    .join(" ");
+
+  title.innerHTML = post.fields.title;
+  date.innerHTML = updatedAt;
+  desc.innerHTML = post.fields.description;
+  time.innerHTML = getReadTime(post.fields.body) + " min read";
+  tags.innerHTML = postTags;
+
+  $("#" + container.id).click(() => {
+    clickedPost = postMap.get(post.sys.id);
+    document.getElementById("blogModalTitle").innerHTML = clickedPost.title;
+    document.getElementById("blogModalBody").innerHTML = converter.makeHtml(
+      clickedPost.body
+    );
+    $("#blogModal").modal("show");
+  });
+}
+
+function getReadTime(body) {
+  const wpm = 200;
+  const words = body.trim().split(/\s+/).length;
+  const time = Math.ceil(words / wpm);
+  return time.toString();
+}
+
+function filterPosts(elem) {
+    var val = $(elem).val();
+
+    $("#postsArchive > li").each(function() {
+        post = JSON.parse(JSON.stringify(postMap.get(this.id)));
+        delete post.body
+        if (JSON.stringify(post).search(val) > -1) {
+            $(this).show();
+        }
+        else {
+            $(this).hide();
+        }
+    });
+}
---
+++ assets/js/github_info.js
@@ -0,0 +1,66 @@
+var get_language_data = [];
+
+httpGetRequest = async (url) => {
+  try {
+    const resp = await axios.get(url, {
+      headers: {
+        // Authorization: `token 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`,
+        "Content-Type": "application/json",
+      },
+    });
+    return await resp.data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+httpGetRequest("https://api.github.com/users/yashgharat/repos").then((res) => {
+  if (res) {
+    Promise.all(
+      res.map((repo) =>
+        httpGetRequest(repo.languages_url).then((languages) =>
+          get_language_data.push(languages)
+        )
+      )
+    ).then(() => {
+      var language_counts = {};
+      var language_portions = {};
+      var total = 0;
+      get_language_data.forEach((repo) => {
+        Object.keys(repo).forEach((language) => {
+          total += repo[language];
+          if (language_counts[language] == null) {
+            language_counts[language] = repo[language];
+          } else {
+            language_counts[language] += repo[language];
+          }
+        });
+      });
+
+      var progressbars = "";
+
+      Object.keys(language_counts).forEach((language) => {
+        portion = ((language_counts[language] / total) * 100).toFixed(0);
+        //language_portions[language] = portion
+        // TODO create progess bars here
+        if (portion > 5) {
+          progressbars +=
+            '<div class="progress">' +
+            '<span class="skill">' +
+            language +
+            ' <i class="val">' +
+            portion +
+            "%</i></span>" +
+            '<div class="progress-bar-wrap">' +
+            '<div class="progress-bar" role="progressbar" aria-valuenow="' +
+            portion +
+            '" aria-valuemin="0" aria-valuemax="100" style="width:' +
+            portion * 2 +
+            '%;"></div>' +
+            "</div></div>";
+          document.querySelector(".github-skills").innerHTML = progressbars;
+        }
+      });
+    });
+  } else console.log("getRepos was null");
+});
